0b2849f4bbb95c82f401b76de1c25bc85343b2a4
hanpengithub <teppeikimura@icloud.com>
2021-11-01 00:18:15 +0900

    できませんでした。

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -23,26 +23,77 @@ void initialize_list(list* l)
 // 先頭にデータを追加
 void push_front(list* l, node* p)
 {
+	if (p == NULL || l == NULL)return;
+	if (l->header == NULL)
+	{
+		l->header = p;
+		l->footer = p;
+	}
+	else
+	{
+		p->pNext = l->header;
+		l->header->pPrev = p;
+		l->header = p;
+	}
+
 }
 
 // 末尾にデータを追加
 void push_back(list* l, node* p)
 {
+	if (p == NULL || l == NULL)return;
+	if (l->footer == NULL)
+	{
+		l->header = p;
+		l->footer = p;
+	}
+	else
+	{
+		p->pPrev = l->footer;
+		l->header->pNext = p;
+		l->footer = p;
+	}
 }
 
 // pのノードを削除
 void remove_node(list* l, node* p)
 {
+	
 }
 
 // pの次のノードを削除
 void remove_next(list* l, node* p)
 {
+	if (l == NULL || p == NULL || p->pNext == NULL)return;
+
+	if (p->pNext->pNext == NULL)
+	{
+		p->pNext = NULL;
+		l->footer = p;
+	}
+	else
+	{
+		p->pNext = p->pNext->pNext;
+		p->pNext->pPrev = p;
+	}
 }
 
 // pの前のノードを削除
 void remove_prev(list* l, node* p)
 {
+	if (l == NULL || p == NULL || p->pPrev == NULL)return;
+
+	if (p->pPrev->pPrev == NULL)
+	{
+		p->pPrev = NULL;
+		l->header = p;
+	}
+
+	else
+	{
+		p->pPrev = p->pPrev->pPrev;
+		p->pPrev->pNext = p;
+	}
 }
 
 
